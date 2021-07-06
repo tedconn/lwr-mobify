34e41c3a8246e77c996c02f2af1386afb43dc297
Ted Conn <tconn@salesforce.com>
2021-07-06 22:49:59 +0200

    fix: using production bundle and requiring the server

--- src/ssr.js
+++ src/ssr.js
@@ -2,7 +2,7 @@
 // This file must be called `ssr.js` and must export a Lambda Node.js compatible handler `get`:
 // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html
 const awsServerlessExpress = require('aws-serverless-express')
-
+const server = require('./app');
 const awsServer = awsServerlessExpress.createServer(server.app)
 
 exports.get = (event, context) => awsServerlessExpress.proxy(awsServer, event, context)
